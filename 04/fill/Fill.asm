0c5f0ca8cba5bd5e04b4b334ffb012386b3685b3
woclass <inkydragon@users.noreply.github.com>
2018-10-04 00:05:36 +0800

    [proj-04] write asm program: fill@one point

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,48 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+//* ----- var and const init ----- *//
+// globle constant
+@SCREEN
+D=A
+@i      // screen ptr
+M=D     // i = SCREEN (16394)
+
+// screen fill type
+@BLACK
+M=1
+@WHITE
+M=0
+
+//* ----- Main function body ----- *//
+(INF_LOOP)
+@KBD
+D=M
+@CLS
+D;JEQ   // if M[KBD] == 0: goto CLS
+
+@FILL
+0;JMP
+
+@INF_LOOP
+0;JMP   // infinite loop
+
+
+//* ----- fill screen ----- *//
+(FILL)
+D=M[BLACK]
+A=M[i]
+M=D     // RAM[i] = BLACK
+
+@INF_LOOP
+0;JMP   // goto INF_LOOP
+
+
+//* ----- clear screen ----- *//
+(CLS)
+D=M[WHITE]
+A=M[i]
+M=D     // RAM[i] = WHITE
+
+@INF_LOOP
+0;JMP   // goto INF_LOOP

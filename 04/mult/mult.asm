ce6c542a2c4de23b80c2e10bbd0ee3b2f50a4f11
woclass <inkydragon@users.noreply.github.com>
2018-10-04 00:01:43 +0800

    [proj-04] write asm program: mult

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,43 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// **
+// * Input: 
+// *    R0 >= 0
+// *    R1 >= 0
+// * Result:
+// *    R2 = R0*R1 < 32768
+@R2
+M=0 // R2 = 0
+
+@R0
+D=M
+@ZERO
+D;JEQ // if R0 == 0: goto ZERO
+
+@R1
+D=M
+@ZERO
+D;JEQ // if R1 == 0: goto ZERO
+
+
+(LOOP)
+@R1
+D=M   // D = R1
+
+@R2
+M=M+D // R2 += D
+
+@R0
+M=M-1 // R0 -= 1
+
+@R0
+D=M
+@LOOP
+D;JGT // if R0 > 0: goto LOOP
+
+
+(ZERO)  // if R0 == 0 | R1 == 0: return 0
+(END)
+@END
+0;JMP //* -- end of program -- *//
